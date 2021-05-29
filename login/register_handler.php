cbb85f3a5c54cb36c7ed28d2e80a9adc50d8494d
Danzel Serrano <danzel.serrano@gmail.com>
2021-05-28 22:24:47 -0400

    User Registration

--- login/register_handler.php
+++ login/register_handler.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * @var string $cleardb_server
+ * @var string $cleardb_username
+ * @var string $cleardb_password
+ * @var string $cleardb_db
+ */
     require('../config.php');
 
     $errors = array('email'=>'','username'=>'','password'=>'');
@@ -37,6 +43,39 @@
 
             if (!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 8) {
                 $errors['password'] = "Password must be at least 8 characters long and should have one upper case letter, one number, and one special character.";
+            }
+        }
+
+        //Check first if user or email already exists in db
+        if(!array_filter($errors)){
+            try{
+                $db = new PDO("mysql:host=$cleardb_server;dbname=$cleardb_db",$cleardb_username,$cleardb_password);
+                $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+                echo "Connected Successfully";
+
+                $stmt = $db->prepare("SELECT * FROM users WHERE email = :email");
+                $r = $stmt->execute(array(":email" => $email));
+                $emailresult = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                if($stmt->rowCount() > 0){
+                    $errors['email'] = "Account already exists under that email address";
+                }
+
+                $stmt = $db->prepare("SELECT * FROM users WHERE username = :username");
+                $r = $stmt->execute(array(":username"=>$username));
+                $userresult = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                if($stmt->rowCount() > 0){
+                    //TODO: Use an api to give suggestions for new usernames
+                    $errors['username'] = "Username is already taken, please choose another";
+                }
+
+                if(!array_filter($errors)){
+                    $stmt = $db->prepare("INSERT INTO users (email,username,password) VALUES (:email,:username,:password)");
+                    $pass_hash = password_hash($password,PASSWORD_BCRYPT);
+                    $r = $stmt->execute(array(":email"=>$email,":username"=>$username,":password"=>$pass_hash));
+                    header("location: login.php");
+                }
+            }catch(PDOException $e){
+                //echo "Connection failed: " . $e->getMessage();
             }
         }
     }
