9838a8fb6ff7b7413c5407b72a02eb2f990a62c9
Rafael McLeary <rafael@rafael-pop.fios-router.home>
2021-06-06 20:54:54 -0400

    modal changes

--- public_html/user.php
+++ public_html/user.php
@@ -114,28 +114,7 @@ if(!isset($_SESSION['user'])){
           Create new post
         </button>
         
-        <!-- Modal -->
-        <div class="modal fade" id="modelId" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content modal-dialog-centered">
-                        <div class="modal-header">
-                                <h5 class="modal-title">Modal title</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                            </div>
-                    <div class="modal-body">
-                        <div class="container-fluid">
-                            Add rows here
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save</button>
-                    </div>
-                </div>
-            </div>
-        </div>
+        
         
         <script>
             $('#exampleModal').on('show.bs.modal', event => {
@@ -147,6 +126,32 @@ if(!isset($_SESSION['user'])){
         </script>
 
         
+    </div>
+
+    <div class="container mt-5">
+    <!-- Modal -->
+        <div class="modal fade" id="modelId" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content modal-dialog-centered">
+                            <div class="modal-header">
+                                    <h5 class="modal-title">Modal title</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                </div>
+                        <div class="modal-body">
+                            <div class="container-fluid">
+                                Add rows here
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    
     </div>
 
 
