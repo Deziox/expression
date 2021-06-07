50c9fdf45ada6ffcf4f96ddfcc4737ecf3c13139
Danzel Serrano <danzel.serrano@gmail.com>
2021-06-07 11:00:44 -0400

    Functionalities for creating new posts

--- s3_config.php
+++ s3_config.php
@@ -5,12 +5,8 @@ $s3 = new Aws\S3\S3Client([
     'version'  => 'latest',
     'region'   => 'us-east-1',
     'credentials' => [
-        'key'    => 'AKIARR2QLCN77CCRK2KR',//getenv('AWS_ACCESS_KEY_ID'),
-        'secret' => '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',//getenv('AWS_SECRET_ACCESS_KEY'),
+        'key'    => getenv('AWS_ACCESS_KEY_ID'),
+        'secret' => getenv('AWS_SECRET_ACCESS_KEY'),
     ]
 ]);
-//$bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
-$bucket = 'expression'?: die('No "S3_BUCKET" config var in found in env!');
-
-//AKIARR2QLCN77CCRK2KR
-//btgDHJMLiuSAsXfxFvGWuIpCWgOtEUsGvrYYA9b4
+$bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
