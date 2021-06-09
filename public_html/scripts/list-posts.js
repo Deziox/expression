a839761352bfc14f62848e3e732d33dc6f53fada
Danzel Serrano <danzel.serrano@gmail.com>
2021-06-08 20:28:26 -0400

    Testing functions not showing on Heroku

--- public_html/scripts/list-posts.js
+++ public_html/scripts/list-posts.js
@@ -1,33 +1,3 @@
-postCardArea = document.querySelector(".post-card-area");
-
-window.onload = function(){
-    postCardArea.innerHTML = "";
-
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST","scripts/get_posts.php",true);
-    xhr.onload = () => {
-        if(xhr.readyState === XMLHttpRequest.DONE){
-            if(xhr.status === 200){
-                let data = xhr.response;
-                if(data == "empty"){
-
-                }else if(data == "nosesh"){
-
-                }else{
-                    //console.log(data);
-                    postCardArea.innerHTML = data;
-                    setUpCommentIntervals();
-                }
-            }
-        }
-    }
-
-    setTimeout(function(){
-    },3000);
-    let formData = new FormData(form);
-    xhr.send(formData);
-};
-
 function sendComment(id){
     var form = document.getElementById('comment_form_' + id);
     var comment = form.querySelector('input');
@@ -108,3 +78,35 @@ function setUpCommentIntervals(){
     });
     //console.log("setUpCommentIntervals " + divs);
 }
+
+
+postCardArea = document.querySelector(".post-card-area");
+
+window.onload = function(){
+    postCardArea.innerHTML = "";
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST","scripts/get_posts.php",true);
+    xhr.onload = () => {
+        if(xhr.readyState === XMLHttpRequest.DONE){
+            if(xhr.status === 200){
+                let data = xhr.response;
+                if(data == "empty"){
+
+                }else if(data == "nosesh"){
+
+                }else{
+                    //console.log(data);
+                    postCardArea.innerHTML = data;
+                    setUpCommentIntervals();
+                }
+            }
+        }
+    }
+
+    setTimeout(function(){
+    },3000);
+    let formData = new FormData(form);
+    xhr.send(formData);
+};
+
