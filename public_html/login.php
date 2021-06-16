28959a8bb6bdb4b69605590de6f3dba04f931af0
Danzel Serrano <danzel.serrano@gmail.com>
2021-06-16 15:20:16 -0400

    Forgot Password Functionality

--- public_html/login.php
+++ public_html/login.php
@@ -14,7 +14,8 @@ include('../login/login_handler.php');
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="stylesheets/style.css">
 </head>
 <body class="logreg">
@@ -22,7 +23,8 @@ include('../login/login_handler.php');
 <div class="container">
     <!-- navbar -->
     <nav id="regNavbar" class="navbar navbar-dark navbar-expand-md py-0 px-5 fixed-top justify-content-around">
-        <a href="index.php" class="navbar-brand"><img class="d-none d-lg-inline col-lg-1" src="imgs/goldensquarelogo.png" alt="">EXPRS</a>
+        <a href="index.php" class="navbar-brand"><img class="d-none d-lg-inline col-lg-1"
+                                                      src="imgs/goldensquarelogo.png" alt="">EXPRS</a>
         <button class="navbar-toggler" data-toggle="collapse" data-target="#navLinks" aria-label="Toggl navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -44,33 +46,33 @@ include('../login/login_handler.php');
                 </div>
                 <div class="col-md-8">
 
-                            <form action="login.php" method="post">
-                <div class="align-self-center mb-2"><h2>Enter Login:</h2></div>
-                <div class="form-row">
-                    <div class="form-group col-md-12">
-                        <label for="Email"><h2">E-mail</h2></label>
-                        <?php echo "<div class=\"error\">".$errors['email']."</div>";?>
-                        <input type="email" class="form-control" id="email" placeholder="name@example.com" name="email">
-                    </div>
-                </div>
-                <div class="form-row">
-                    <div class="form-group col-md-12">
-                        <label for="password"><h2>Password</h2></label>
-                        <?php echo "<div class=\"error\">".$errors['password']."</div>";?>
-                        <input type="password" class="form-control" id="password" placeholder="Password" name="password">
-                    </div>
-                </div>
+                    <form action="login.php" method="post">
+                        <div class="align-self-center mb-2"><h2>Enter Login:</h2></div>
+                        <div class="form-row">
+                            <div class="form-group col-md-12">
+                                <label for="Email"><h2">E-mail</h2></label>
+                                <?php echo "<div class=\"error\">" . $errors['email'] . "</div>"; ?>
+                                <input type="email" class="form-control" id="email" placeholder="name@example.com"
+                                       name="email">
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-12">
+                                <label for="password"><h2>Password</h2></label>
+                                <?php echo "<div class=\"error\">" . $errors['password'] . "</div>"; ?>
+                                <input type="password" class="form-control" id="password" placeholder="Password"
+                                       name="password">
+                            </div>
+                        </div>
 
-                <button type="submit" class="btn btn-primary" name="submit">Submit</button>
-
-        </div>
-</div>
-</form>
-
+                        <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+                    </form>
+                    <br>
+                    <a type="button" class="btn btn-info" style="font-size: small" href="forgot_password.php">Forgot Your Password?</a>
                 </div>
             </div>
-         
-
+        </div>
+</div>
 
 </div>
 </section>
@@ -78,9 +80,15 @@ include('../login/login_handler.php');
 
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
 
 </body>
 </html>
